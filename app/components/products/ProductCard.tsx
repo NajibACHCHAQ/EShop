c3342e6cd8f3c551fc0a86a406bbb20e1fc30780
NajibACHCHAQ <najib.achchaq@gmail.com>
2024-01-12 00:01:57 +0100

    implementation page product

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -4,13 +4,16 @@ import Image from 'next/image'
 import { truncateText } from '@/utils/truncateText'
 import { formatPrice } from '@/utils/formatPrice'
 import { Rating } from '@mui/material'
+import { useRouter } from 'next/navigation'
 interface ProductCardProps{
   data:any
 }
 export const ProductCard:React.FC<ProductCardProps> = ({data}) => {
+  const router = useRouter()
   const productRating = data.reviews.reduce((acc:number,item:any) => item.rating + acc,0) / data.reviews.length;
+  console.log(productRating)
   return (
-    <div className='col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition:scale-105 text-center text-sm'>
+    <div onClick={()=> router.push(`/product/${data.id}`)} className='col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition:scale-105 text-center text-sm'>
       <div className='flex flex-col items-center w-full gap-1'>
         <div className='aspect-square overflow-hidden relative w-full'>
           <Image fill src={data.images[0].image} className='w-full h-full object-contain' alt={data.name}/>
---
+++ app/product/[productId]/ProductDetails.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { Rating } from '@mui/material'
+import React from 'react'
+interface ProductDetailsProps{
+    product:any
+}
+
+const Horizontal =()=>{
+    return<hr className='w-[30% my-2]'/>
+}
+export const ProductDetails:React.FC<ProductDetailsProps> = ({product}) => {
+    const productRating = product.reviews.reduce((acc:number,item:any) => item.rating + acc,0) / product.reviews.length;
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
+        <div>Images</div>
+        <div className='flex flex-col gap-1 text-slate-500 text-sm'>
+            <h2 className='text-3xl font-medium text-slate-700'>{product.name}</h2>
+            <div className='flex items-center gap-2'>
+                <Rating value={productRating} readOnly/>
+                <div>{product.reviews.length} Avis</div>
+            </div>
+            <Horizontal/>          
+            <div className='text-justify'>{product.description}</div>
+            <Horizontal/>
+            <div>
+                <span className='font-semibold'>CATEGORIE : </span>{product.category}
+            </div>
+            <div>
+                <span className='font-semibold'>MARQUE : </span>{product.BRAND}
+            </div>
+            <div>
+                {/* {product.inStock ? 'En stock' : 'Rupture de stock'} */}
+                {product.quantity >0 ? 'En stock ' + product.quantity : 'Rupture de stock'}
+            </div>
+        </div>
+    </div>
+  )
+}
