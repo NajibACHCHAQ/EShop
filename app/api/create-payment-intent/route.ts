bfe0012ce6db509ee563cc5ab588b27d722a07c5
NajibACHCHAQ <najib.achchaq@gmail.com>
2024-01-21 19:45:27 +0100

    Doublon order lors de la creation d'une commande

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -6,7 +6,7 @@ import { getCurrentUser } from "@/actions/GetCurrentUser";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     apiVersion: "2023-10-16"
-});
+})
 
 const calculateOrderAmount = (items: CartProductType[]) => {
     const totalPrice = items.reduce((acc, item) => {
@@ -14,22 +14,24 @@ const calculateOrderAmount = (items: CartProductType[]) => {
         return acc + itemTotal;
     }, 0);
     
+
     return totalPrice;
-};
+}
 
 export async function POST(request: Request) {
+    // Vérifie l'utilisateur actuel
     const currentUser = await getCurrentUser();
 
     if (!currentUser) {
-        console.log("Unauthorized user");
         return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
     }
 
+    // Récupère les données du corps de la requête
     const body = await request.json();
     const { items, payment_intent_id } = body;
     const total = calculateOrderAmount(items) * 100;
-    console.log("Total Order Amount:", total);
 
+    // Prépare les données de la commande
     const orderData = {
         user: { connect: { id: currentUser.id } },
         amount: total,
@@ -37,21 +39,19 @@ export async function POST(request: Request) {
         status: 'pending',
         deliveryStatus: 'pending',
         paymentIntentId: payment_intent_id,
-        products: items
+        products: items,
     };
 
     if (payment_intent_id) {
-        console.log("Updating payment intent:", payment_intent_id);
-
+        // Si un identifiant de paiement est fourni, met à jour le paiement intent
         const current_intent = await stripe.paymentIntents.retrieve(payment_intent_id);
-        if (current_intent) {
-            console.log("Current Payment Intent:", current_intent);
 
+        if (current_intent) {
             const updated_intent = await stripe.paymentIntents.update(
                 payment_intent_id, { amount: total }
             );
-            console.log("Updated Payment Intent:", updated_intent);
 
+            // Met à jour la commande correspondante dans la base de données
             const [existing_order, update_order] = await Promise.all([
                 prisma.order.findFirst({
                     where: { paymentIntentId: payment_intent_id }
@@ -60,34 +60,29 @@ export async function POST(request: Request) {
                     where: { paymentIntentId: payment_intent_id },
                     data: {
                         amount: total,
-                        products: items
-                    }
-                })
+                        products: items,
+                    },
+                }),
             ]);
 
-            console.log("Existing Order:", existing_order);
-            console.log("Update Order:", update_order);
-
             if (!existing_order) {
-                console.log("Invalid payment Intent");
                 return NextResponse.json({ error: 'Invalid payment Intent' }, { status: 400 });
             }
 
             return NextResponse.json({ paymentIntent: updated_intent });
         }
     } else {
-        console.log("Creating new payment intent");
-
+        // Si aucun identifiant de paiement n'est fourni, crée un nouveau paiement intent
         const paymentIntent = await stripe.paymentIntents.create({
             amount: total,
             currency: 'eur',
-            automatic_payment_methods: { enabled: true }
+            automatic_payment_methods: { enabled: true },
         });
 
-        console.log("New Payment Intent:", paymentIntent);
-
+        // Associe l'identifiant du paiement intent à la commande
         orderData.paymentIntentId = paymentIntent.id;
 
+        // Crée une nouvelle commande dans la base de données
         await prisma.order.create({
             data: orderData,
         });
