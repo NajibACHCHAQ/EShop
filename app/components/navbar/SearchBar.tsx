df982368b075305762b3bacb1f1e37f828b6736b
NajibACHCHAQ <najib.achchaq@gmail.com>
2024-01-28 18:43:44 +0100

    display product from searchbar to fix

--- app/components/navbar/SearchBar.tsx
+++ app/components/navbar/SearchBar.tsx
@@ -1,11 +1,49 @@
 'use client'
+import { useRouter } from 'next/navigation'
+import queryString from 'query-string'
 import React from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 
 export const SearchBar = () => {
+
+    const router = useRouter() 
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState:{
+            errors
+        }
+    } = useForm<FieldValues>({
+        defaultValues:{
+            searchTerm: ''
+        }
+    })
+
+    const onSubmit:SubmitHandler<FieldValues> = async (data) =>{
+        console.log(data)
+
+        if(!data.searchTerm) return router.push('/')
+        const url = queryString.stringifyUrl(
+            {
+                url:'/',
+                query:{
+                searchTerm: data.searchTerm
+            }
+        },{skipNull:true})
+
+        console.log(url)
+
+        router.push(url)
+        reset()
+    }
+
+
   return (
     <div className='flex items-center'>
-        <input autoComplete='off' placeholder='Explore la boutique' type="text" className='p-2 rounded-l-md border-gray-300 rounded-1-md focus:outline-none focus:border-[0.5px] focus:border-slate-500 w-80' />
-        <button className='bg-slate-700 hover:opacity-80 text-white p-2 rounded-r-md'>Recherche</button>
+        <input {...register('searchTerm')} autoComplete='off' placeholder='Explore la boutique' type="text" className='p-2 rounded-l-md border-gray-300 rounded-1-md focus:outline-none focus:border-[0.5px] focus:border-slate-500 w-80' />
+        <button onClick={handleSubmit(onSubmit)} className='bg-slate-700 hover:opacity-80 text-white p-2 rounded-r-md'>Recherche</button>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -35,11 +35,14 @@ export default async function Home({searchParams}:HomeProps) {
         </div>
 
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {shuffleProducts.map((product: any )=>{
-            return <div>
-              <ProductCard data={product}/>
+        {shuffleProducts.map((product: any) => {
+          return (
+            <div key={product.id}>
+              <ProductCard data={product} />
             </div>
-          })}
+          );
+        })}
+
         </div>
       </Container>
     </div>
