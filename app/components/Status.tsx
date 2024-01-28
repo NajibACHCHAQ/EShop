4cc0b7de156ca3e007f99363af50e9104aa6d072
NajibACHCHAQ <najib.achchaq@gmail.com>
2024-01-29 00:41:15 +0100

    get deliveryadress to fix

--- app/components/Status.tsx
+++ app/components/Status.tsx
@@ -11,7 +11,7 @@ interface StatusProps{
 
 export const Status:React.FC<StatusProps> = ({text,icon:Icon,bg,color}) => {
   return (
-    <div className={`${bg} ${color} px-rounded flex items-center gap-1 p-1 w-[100px] justify-center`}>
+    <div className={`${bg} ${color} px-rounded flex items-center gap-1 p-1 pr-2 pl-2 w-[fit-content] justify-center`}>
         {text}<Icon size={15}/>
     </div>
   )
